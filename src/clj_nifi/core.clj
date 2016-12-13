4ee87ce53b789a253560bdf14bf0380bd5022730
Goran Jovic <goranjovic@gmail.com>
2016-12-13 18:33:39 +0100

    basic one file in, one file out session functions

--- src/clj_nifi/core.clj
+++ src/clj_nifi/core.clj
@@ -1,6 +1,8 @@
 (ns clj-nifi.core
   (:import (org.apache.nifi.components PropertyDescriptor$Builder)
-           (org.apache.nifi.processor Relationship$Builder)))
+           (org.apache.nifi.processor Relationship$Builder ProcessSession)
+           (org.apache.nifi.flowfile FlowFile)
+           (org.apache.nifi.processor.io OutputStreamCallback)))
 
 (defn relationship [& {:keys [name description auto-terminate]
                        :or {name           "Relationship"
@@ -39,3 +41,38 @@
       (add-validators validators)
       (.allowableValues (if allowable-values (set allowable-values)))
       (.build)))
+
+(defn init [context session]
+  {:context context :session session})
+
+(defn create [{:keys [session]}]
+  {:session session
+   :file (.create session)})
+
+(defn get-one [{:keys [session]}]
+  {:session session
+   :file (.get session)})
+
+(defn penalize [{:keys [session file]}]
+  {:session session
+   :file (.penalize session file)})
+
+(defn put-attribute [{:keys [session file]} k v]
+  {:session session
+   :file (.putAttribute session file k v)})
+
+(defn remove-attribute [{:keys [session file]} k]
+  {:session session
+   :file (.removeAttribute session file k)})
+
+(defn output-callback [contents]
+  (reify OutputStreamCallback
+    (process [_ out]
+      (spit out contents))))
+
+(defn demo [session]
+  (-> (init nil session)
+      create
+      (put-attribute "a" "123")
+      (put-attribute "b" "abc")
+      (remove-attribute "a")))
